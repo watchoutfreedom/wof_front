4a239f222275df61e840d9e4627e5be38cf5bb92
jano <alex@worldx3.com>
2024-09-23 19:00:27 +0200

    Clean wp.

--- components/mailchimp.php
+++
@@ -1,9 +0,0 @@
-<div id="mce-responses" class="clear sms-confirm">
-    <div class="response" id="mce-error-response" style="display:none"></div>
-    <div class="response" id="mce-success-response" style="display:none"></div>
-  </div>   <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-
-  <form action="//madridfree.us7.list-manage.com/subscribe/post?u=61f52d4983185f6f2fe2645eb&amp;id=93c43162fd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-   <input type="email" placeholder="tu@correo.com" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-   <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-</form>
