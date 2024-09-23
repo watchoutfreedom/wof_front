f10db11769d974bba8ec4576c27905c0a1ccc29f
jano <alex@worldx3.com>
2024-09-23 18:46:08 +0200

    edited home.

--- templates/shop.php
+++ templates/shop.php
@@ -10,7 +10,7 @@ get_header();
 
 <div class="wrap">
   <div class="wrap__box wrap__box--shop">
-    <?php $the_query = new WP_Query('showposts=24&category_name=shop'); while ($the_query->have_posts()) : $the_query->the_post();?>
+    <?php $the_query = new WP_Query('showposts=24&category_name=tienda'); while ($the_query->have_posts()) : $the_query->the_post();?>
       <?php get_template_part('components/product'); ?>
     <?php endwhile; ?>
   </div>
