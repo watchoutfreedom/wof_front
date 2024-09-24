eb7f11c92b5f3b2c41b218ee453231a29a35284a
jano <alex@worldx3.com>
2024-09-24 19:08:26 +0200

    Edited list os posts.

--- components/list-debate.php
+++ components/list-debate.php
@@ -1,4 +1,4 @@
-<div class="wrap">
+<div class="wrap wrap--list">
     <div class="wrap__box">
       <h3 class="wrap__title">Más debates</h3>
       <?php $the_query = new WP_Query('showposts=4&category_name=debate'); while ($the_query->have_posts()) : $the_query->the_post();?>
