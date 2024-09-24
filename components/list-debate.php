d9f796a7afe3779f7780734714e5521ed0e4e0e8
jano <alex@worldx3.com>
2024-09-24 19:09:18 +0200

    test debate.

--- components/list-debate.php
+++ components/list-debate.php
@@ -2,9 +2,7 @@
     <div class="wrap__box">
       <h3 class="wrap__title">Más debates</h3>
       <?php $the_query = new WP_Query('showposts=4&category_name=debate'); while ($the_query->have_posts()) : $the_query->the_post();?>
-      <div class="home__post">
           <?php get_template_part('components/article-list'); ?>
         </div>
       <?php endwhile; ?>
-    </div>
 </div>
