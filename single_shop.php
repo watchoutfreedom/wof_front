cf01166e9ddf183c9a11b8abdb793005307e0b85
jano <alex@worldx3.com>
2024-09-24 17:09:40 +0200

    Shop template:

--- single_shop.php
+++ single_shop.php
@@ -2,18 +2,22 @@
 
 <article class="product">
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-			<?php the_post_thumbnail('fotogrande'); ?>
-
-			<h1 class="single-noticias__title">
-			<?php the_title(); ?>
-      </h1>
-			
-			<?php bloginfo('get_cat_name'); ?>
-
-			<div class="main single-noticias__main">
-         <?php the_content("Sigue leyendo"); ?>
-      </div>
+			<div class="product__content">
+				<?php the_post_thumbnail('fotogrande'); ?>
+				<div class="main single-noticias__main">
+					<?php the_content("Sigue leyendo"); ?>
+				</div>
+			</div>
+			<div class="product__sidebar">
+				<div class="product__sticky">
+					<h1 class="single-noticias__title">
+						<?php the_title(); ?>
+					</h1>
+					<div>40,00 €</div>
+					<a href="" class="product__btn">Comprar</a>
+				</div>
+			</div>
+	
 
 			<?php endwhile; else: ?>
     <?php include (TEMPLATEPATH . '/404.php'); ?>
