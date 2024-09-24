30fabdbdc7db4489c1a1b8858414902c3519e100
jano <alex@worldx3.com>
2024-09-24 16:52:48 +0200

    fix debate template.

--- header.php
+++ header.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html xmlns:fb="http://ogp.me/ns/fb#" lang="es">
-<head>
-  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" type="image/png">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title><?php bloginfo('title'); ?></title>
-  <meta name="description" content="<?php bloginfo('description'); ?>"/>
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-  <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-  <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-  <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
-  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/scss/main.css" as="styles">
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+<?php get_template_part('components/head'); ?>
+
 <header class="header">
   <div class="header__wrap">
     <div class="header__logo">
--- single_shop.php
+++ single_shop.php
@@ -1,48 +1,24 @@
 <?php get_header(); ?>    
-<main id="post">
-	<article class="single-default">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<div class="single-default__content">
-				<div class="single-hero">
-					<header class="header">
-							<div class="header__wrap">
-								<div class="header__logo">
-									<a href="<?php bloginfo('url'); ?>" class="header__logo-link">
-										WOF
-									</a>
-								</div>
-								<div class="header__btn">
-									<a href="">Colabora con WOF</a>
-								</div>
-							</div>
-						</header>
-						<div class="single-default__presentation">
-							<div class="single-default__category">
-								<?php get_template_part('atoms/category-link'); ?>
-							</div>
-							<h1 class="single-default__title">
-								<?php the_title(); ?>
-							</h1>
-							<div class="single-default__intro">
-								Un repaso al impacto que ha generado el uso de herramientas nocode, o lo que es lo mismo, herramientas que permiten a un usuario lanzar una web sin saber código
-							</div>
-						</div>
-				</div>
-				<div class="single-default__meta">
-					<?php get_template_part('atoms/meta'); ?>
-				</div>
-				<div class="main single-default__main">
-					<?php the_content("Sigue leyendo"); ?>
-				</div>
-				<div class="single__tags">
-					<?php the_tags('<span>Palabras clave</span>', '', ''); ?>
-				</div>
-			</div>
-		<?php endwhile; else: ?>
-			<?php include (TEMPLATEPATH . '/404.php'); ?>
-		<?php endif; ?>
-	</article>
-</main>
+
+<article class="product">
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+			<?php the_post_thumbnail('fotogrande'); ?>
+
+			<h1 class="single-noticias__title">
+			<?php the_title(); ?>
+      </h1>
+			
+			<?php bloginfo('get_cat_name'); ?>
+
+			<div class="main single-noticias__main">
+         <?php the_content("Sigue leyendo"); ?>
+      </div>
+
+			<?php endwhile; else: ?>
+    <?php include (TEMPLATEPATH . '/404.php'); ?>
+  <?php endif; ?>
+</article>
 
 <?php get_template_part('components/last-posts'); ?>
 
