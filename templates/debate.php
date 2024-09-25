bbde6ca2e5f8a07db50e6399f8a48cf43b2ce375
watchoutfreedom <watchoutfreedom@gmail.com>
2024-09-25 04:21:20 +0200

    bote

--- templates/debate.php
+++ templates/debate.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 <div class="hero">
   <h1 class="hero__title">Debate</h1>
-  <div class="hero__description">Bote de este mes: 500 €</div>
+  <div class="hero__description">Bote del mes: 500 €</div>
 </div>
 
 <div class="wrap">
