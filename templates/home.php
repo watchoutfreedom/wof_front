456a968dbedf5f7242b66564e1f3a5aab4c7196e
watchoutfreedom <watchoutfreedom@gmail.com>
2024-09-25 21:16:19 +0200

    title

--- templates/home.php
+++ templates/home.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 <div class="home">
 <div class="hero">
-    <h1 class="hero__title">Bienvenido al otro lado</h1>
+    <h1 class="hero__title">Bienvenidos al otro lado.</h1>
     <div class="hero__description">Fomentamos la cooperación social, el debate y el conocimiento en materias afectadas por la desinformación o el desinterés del statu quo.</div>
   </div>
   <div class="wrap">
