2329fb2f4e01e72d06fb232e9ab08cd3d45520f8
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-09-27 14:36:01 +0200

    answer to

--- components/article-list.php
+++ components/article-list.php
@@ -8,7 +8,8 @@
   </div>
   <div class="article-list__content">
     <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="article-list__link">
-      <div class="article-list__date"> <?php the_time('d F Y'); ?></div></a>
+      <div class="article-list__date"> <?php the_time('d F Y'); ?></div>
+    </a>
       <div class="article-list__responses"><?php if($answer_to = get_field('answer_to')) echo " Respuesta a <a class='answer__to' href=".get_permalink($answer_to).">".get_the_title($answer_to)."</a>";?></div>
       <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="article-list__link">
         <h3 class="article-list__title">
