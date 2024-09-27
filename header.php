a267129fb151dbd949487eb42d67b80c30b1af6b
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-09-27 19:16:28 +0200

    fix nav

--- header.php
+++ header.php
@@ -8,7 +8,15 @@
       </a>
     </div>
     <nav class="header__nav">
-    <ul><?php wp_list_pages('title_li= '); ?></ul>
+    <ul>
+      <?php 
+       $args = array(
+        'exclude' => get_page_by_path("create-post").",".get_page_by_path("login").",".get_page_by_path("sign-up"), // Replace 5 with the parent page ID
+        'title_li' => ''
+        );
+      wp_list_pages($args); 
+      ?>
+    </ul>
     </nav>
     <div class="header__btn">
       <a href="/colabora">Colabora con WOF</a>
