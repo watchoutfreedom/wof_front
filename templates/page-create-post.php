635292a95bbf918af30080a9e3adf50e6d1bd3d9
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-09-27 15:31:11 +0200

    fix 404

--- templates/page-create-post.php
+++ templates/page-create-post.php
@@ -5,8 +5,73 @@
 */
 
 
+$post = get_post($_GET['id']);
+
+//add redirect if user can't edit this post
+if(!is_user_logged_in()){
+    set_transient( 'originalRegisterRefererURL', $_SERVER['HTTP_REFERER'], 60 * 60 * 24 );
+    wp_redirect( wp_login_url() );
+}
+
+if(!(wp_get_current_user()->ID == $post->post_author) 
+    && !current_user_can( 'edit_others_posts') 
+    && $_GET['action'] == 'edit')
+    wp_redirect(home_url());
+
+if(wp_get_current_user()->ID == $post->post_author && $_GET['action'] == 'create')
+    wp_redirect(get_permalink($_GET['id']));
+
+
+acf_form_head();
 
 get_header(); ?>
 
+<div class="wrap">
+    <?php
+
+    if( isset($_GET['id']) && isset($_GET['action']) && $_GET['action'] == 'edit' ){
+
+        acf_form(array(
+            'post_id'       => $_GET['id'],
+            'post_title'    => true,
+            'post_content'  => false,
+            'post_category' => array(28),
+            'return' => '%post_url%',
+            'submit_value' => __("Publicar", 'acf')
+        )); 
+    }
+    else{
+
+        if($_GET['action'] == "create" && isset($_GET['id']))
+            echo "<div class='excerp'>Responder a <a class='answer__to' href='".get_permalink($_GET['id'])."'>".get_the_title($_GET['id'])."</a></div>";
+
+        acf_form(array(
+            'post_id'       => 'new_post',
+            'post_title'    => true,
+            'post_content'  => false,
+            'return' => '%post_url%',
+            'submit_value' => __("Publicar", 'acf'),
+            'new_post'      => array(
+                'post_type'     => 'post',
+                'post_category' => array(28),
+                'post_status'   => 'publish'
+            )
+        ));
+    }
+    
+    ?>
+</div>
+
+<script>
+        
+    jQuery("form").submit(function(e){
+
+        if(tinymce.activeEditor.getContent({format : 'text'}).length < 500){
+        e.preventDefault();
+        e.stopPropagation(); // Prevent the acf object from ever hearing about this form submission
+        alert("Respuesta no tiene un mínimo 500 caracteres");
+        }
+        });
+</script>
 
 <?php wp_footer() ?>
