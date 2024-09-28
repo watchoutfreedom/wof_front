62b257800282d6d3a005c651d7e3fc908bfca360
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-09-28 18:33:17 +0200

    infinite scroll

--- templates/debate.php
+++ templates/debate.php
@@ -22,7 +22,7 @@ get_header();
 
 <div class="wrap">
   <div class="wrap__box wrap__box--debate">
-    <?php echo do_shortcode('[ajax_load_more id="5545649519" loading_style="infinite classic" post_type="post" posts_per_page="5"]')?>
+    <?php echo do_shortcode('[ajax_load_more post_type="post" posts_per_page="4" transition_container_classes="wrap__box--debate"]')?>
   </div>
 </div>
 
