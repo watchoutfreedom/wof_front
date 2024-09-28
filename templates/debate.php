a926f66e3e64e3ddcd234252a992ec20fb31d1f4
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-09-28 18:09:42 +0200

    infinite scroll

--- templates/debate.php
+++ templates/debate.php
@@ -14,11 +14,19 @@ get_header();
 
 <div class="wrap">
   <div class="wrap__box wrap__box--debate">
+    <?php $the_query = new WP_Query('showposts=12&category_name=debate'); while ($the_query->have_posts()) : $the_query->the_post();?>
+      <?php get_template_part('components/article-list'); ?>
+    <?php endwhile; ?>
+  </div>
+</div>
+
+<div class="wrap">
+  <div class="wrap__box wrap__box--debate">
     <?php echo do_shortcode('[ajax_load_more id="5545649519" loading_style="infinite classic" post_type="post" posts_per_page="5"]')?>
   </div>
 </div>
-<?php 
-?>
+
+
 <?php get_template_part('components/colabora'); ?>
 
 <?php get_footer(); ?>
