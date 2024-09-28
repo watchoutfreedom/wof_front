61a881bd7f28c0a24eb0725579293a3bb9b9c3f8
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-09-28 16:12:14 +0200

    bug fix

--- functions/scripts.php
+++ functions/scripts.php
@@ -6,7 +6,7 @@ function register_scripts() {
 function enqueue_scripts() {
   wp_enqueue_style('theme-style');
   wp_enqueue_script('app');
-  wp_register_script( 'altcha', 'https://eu.altcha.org/js/latest/altcha.min.js' ) );
+  wp_register_script( 'altcha', 'https://eu.altcha.org/js/latest/altcha.min.js' ) ;
 }
 add_action('wp_enqueue_scripts', 'register_scripts');
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
