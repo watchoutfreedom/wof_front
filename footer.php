c7154624bb52f9c9b0f9f8ec14c6e140085d8dc1
jano <alex@worldx3.com>
2024-10-01 14:15:49 +0200

    Login usuario.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-  <p>2024 © Watch Out, Freedom!</p>
+  <p>2024 © Watch Out, Freedom! — <a href="<?php bloginfo('url'); ?>/login">Login</a></p>
 </footer>
 
 <?php wp_footer(); ?>
