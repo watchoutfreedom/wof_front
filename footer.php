af4f9fb9202cf3b1481bb27a03112d9e82941a5d
jano <alex@worldx3.com>
2024-10-02 12:47:46 +0200

    Edited footer.

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
   <p>2024 © Watch Out, Freedom! — <a href="<?php bloginfo('url'); ?>/login">Login</a></p>
+  <p class="mail">mail@watchoutfreedom.com</p>
 </footer>
 
 <?php wp_footer(); ?>
