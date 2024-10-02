25e0c787ea8df1c2a34ff9190a831c52884c04e2
jano <alex@worldx3.com>
2024-10-02 10:43:15 +0200

    Update meta.

--- atoms/meta.php
+++ atoms/meta.php
@@ -3,7 +3,7 @@
     <?php echo get_avatar( get_the_author_meta('email'), '42' ); ?> 
     <div class="meta__content">
       <?php the_author_posts_link(); ?>
-      <p><?php the_author_description(); ?></p>
+      <p><?php get_the_author_description(); ?></p>
       <a href="<?php the_field('field_6534082571f05'); ?>">Donar</a>
     </div>
   </div>
