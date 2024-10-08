8f819d97af233b6a62c973c6c8831288eed29469
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-10-08 10:50:52 +0100

    fix cat archive

--- category.php
+++ category.php
@@ -1,27 +1,3 @@
-<?php get_header(); ?>
-
-<section class="section page-sidebar">
-  <div class="page-sidebar__content cat-cat">
-    <div class="page-sidebar__cat">
-      <h1 class="page-sidebar__cat-title">
-        <?php single_cat_title(); ?> 
-      </h1>
-    </div>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <?php get_template_part('components/article-list'); ?>
-    <?php endwhile; ?>
-  </div>
-</section>
-
-<?php get_template_part('components/category-nav'); ?>
-
-<?php  else: ?>
-	<?php _e('Lo sentimos, no hay resultados con este término de búsqueda.'); ?>
-<?php endif; ?>
-
-<?php get_footer(); ?>
-
-
 <?php
 /*
 Template Name: debate
