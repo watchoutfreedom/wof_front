0972378905e30a2522c4c419f5f0bfaa9b00555d
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-09 14:38:34 +0200

    shop

--- templates/shop.php
+++ templates/shop.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 <div class="hero">
   <h1 class="hero__title">Tienda online</h1>
-  <div class="hero__description">Impulsa nuestras iniciativas.</div>
+  <div class="hero__description">Impulsa las iniciativas.</div>
 </div>
 
 <div class="wrap">
