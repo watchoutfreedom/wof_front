97aa96e4750a66137d1097917f8cb3c2b141d058
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-10-15 15:19:06 +0100

    add author favicon

--- author.php
+++ author.php
@@ -4,9 +4,11 @@ Template Name: debate
 */
 get_header();
 ?>
-<div class="hero">
-  <h1 class="hero__title"><?php the_author(); ?> (<?php the_author_posts(); ?></h1>
-</div>
+<h1 class="hero__title">
+  <?php echo get_avatar(get_the_author_ID(), 64); ?>
+  <?php the_author(); ?> (<?php the_author_posts(); ?>)
+</h1>
+
 <div class="wrap">
   <div class="wrap__box wrap__box--debate">
   <?php echo do_shortcode('[ajax_load_more id="8488605913" loading_style="infinite ring" author="'.get_the_author_ID().'" post_type="post" posts_per_page="8" category="debate" transition_container_classes="wrap__box--debate"]')?>
