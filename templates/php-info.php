90bed87f517e3308ed3899b364d6a844ae7524cd
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-10-15 16:33:13 +0100

    only show php info if admin

--- templates/php-info.php
+++ templates/php-info.php
@@ -7,12 +7,12 @@ if ( is_user_logged_in() )
     wp_redirect(home_url());
 
 
-    if (current_user_can('administrator')) {
+    if (current_user_can('manage_options')) {
       phpinfo();
     } else {
       // Display a message or redirect to a different page
       echo 'You do not have permission to view the PHP info page.';
     }
-    
+
     ?>
     
