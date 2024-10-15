3c0c99ac5f71fd30b64ad544f2b9ecff21b1e32c
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-10-15 16:34:10 +0100

    same as last

--- templates/php-info.php
+++ templates/php-info.php
@@ -3,15 +3,12 @@
 Template Name: Php info
 */
 
-if ( is_user_logged_in() )
-    wp_redirect(home_url());
 
 
     if (current_user_can('manage_options')) {
       phpinfo();
     } else {
-      // Display a message or redirect to a different page
-      echo 'You do not have permission to view the PHP info page.';
+      wp_redirect(home_url());
     }
 
     ?>
