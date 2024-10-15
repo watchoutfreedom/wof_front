197c65c8caa23cef93e0821634306ffeecb9e977
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-10-15 16:30:30 +0100

    php info

--- templates/php-info.php
+++ templates/php-info.php
@@ -7,12 +7,12 @@ if ( is_user_logged_in() )
     wp_redirect(home_url());
 
 
-    <?php
     if (current_user_can('administrator')) {
       phpinfo();
     } else {
       // Display a message or redirect to a different page
       echo 'You do not have permission to view the PHP info page.';
     }
+    
     ?>
     
