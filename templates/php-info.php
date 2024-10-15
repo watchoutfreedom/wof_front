d8ec1346547e0618abf30d8a0b5f788d0b440371
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-10-15 16:39:36 +0100

    test mail

--- templates/php-info.php
+++ templates/php-info.php
@@ -3,10 +3,37 @@
 Template Name: Php info
 */
 
+if ( function_exists( 'mail' ) )
+{
+    echo 'mail() is available';
+}
+else
+{
+    echo 'mail() has been disabled';
+}
+
+$email = "brunoxpalma@gmail.com";
+$subject =  "Email Test";
+$message = "this is a mail testing email function on server";
+
+
+$sendMail = mail($email, $subject, $message);
+if($sendMail)
+{
+echo "Email Sent Successfully";
+}
+else
+
+{
+echo "Mail Failed";
+}
+
 
 
     if (current_user_can('manage_options')) {
       phpinfo();
+
+      
     } else {
       wp_redirect(home_url());
     }
