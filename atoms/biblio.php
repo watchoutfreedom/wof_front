f2d2e159ae886285f17a9865c45d160f7841de8b
jano <alex@worldx3.com>
2024-10-16 11:12:48 +0200

    book

--- atoms/biblio.php
+++ atoms/biblio.php
@@ -1,14 +1,16 @@
 <div class="bibliografia">
 
-<?php
-$rows = get_field('field_637531d2abd69' );
-if( $rows ) {
-    $index = array_rand( $rows );
-    $rand_row = $rows[ $index ];
-    $rand_row_title = $rand_row['title'];
-    // Do something...
-}
-Link to heading# ?>
+<?php if( have_rows('field_637531d2abd69') ): ?>
+    <ul class="slides">
+    <?php while( have_rows('field_637531d2abd69') ): the_row(); 
+        $child_title = get_sub_field('field_637531efabd6a');
+        ?>
+        <li>
+            <?php echo ( $child_title ); ?>
+        </li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
 
     
 </div>
