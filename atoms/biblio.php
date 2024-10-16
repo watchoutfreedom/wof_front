9b4b447bab5c0242b25ba5ba628a44c6b6de8b08
jano <alex@worldx3.com>
2024-10-16 11:09:40 +0200

    Update

--- atoms/biblio.php
+++ atoms/biblio.php
@@ -1,13 +1,15 @@
-<?php if( have_rows('field_637531d2abd69') ): ?>
-    <ul class="slides">
-    <?php while( have_rows('field_637531efabd6a') ): the_row(); 
-        $image = get_sub_field('image');
-        ?>
-        <li>
-            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
-            <p><?php echo acf_esc_html( get_sub_field('caption') ); ?></p>
-        </li>
-    <?php endwhile; ?>
-    </ul>
-<?php endif; ?>
+<div class="bibliografia">
+  <?php if( have_rows('bibliography') ): ?>
+      <ul class="slides">
+      <?php while( have_rows('book') ): the_row(); 
+          $image = get_sub_field('image');
+          ?>
+          <li>
+              <?php echo wp_get_attachment_image( $image, 'full' ); ?>
+              <p><?php echo acf_esc_html( get_sub_field('caption') ); ?></p>
+          </li>
+      <?php endwhile; ?>
+      </ul>
+  <?php endif; ?>
+</div>
 
