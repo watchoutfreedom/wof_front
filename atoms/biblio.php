e2d547cfc2a37801a7d64a0ee8399ad12935c1da
jano <alex@worldx3.com>
2024-10-16 11:13:40 +0200

    Update

--- atoms/biblio.php
+++ atoms/biblio.php
@@ -1,7 +1,8 @@
 <div class="bibliografia">
 
 <?php if( have_rows('field_637531d2abd69') ): ?>
-    <ul class="slides">
+  <h2>Bibliografía</h2>
+  <ul class="slides">
     <?php while( have_rows('field_637531d2abd69') ): the_row(); 
         $child_title = get_sub_field('field_637531efabd6a');
         ?>
