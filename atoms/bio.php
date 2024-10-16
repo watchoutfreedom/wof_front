24c13d0a4226ecdaeecb34ccf3dd60fc7e48f299
jano <alex@worldx3.com>
2024-10-16 11:25:37 +0200

    Update bio.

--- atoms/bio.php
+++ atoms/bio.php
@@ -2,7 +2,7 @@
   <div class="meta__author">
     <?php echo get_avatar( get_the_author_meta('email'), '42' ); ?> 
     <div class="meta__content">
-      <?php the_author_link(); ?>
+      <div class="meta__name-link"><?php the_author_link(); ?></div>
       <p><?php the_author_description(); ?></p>
       <a href="<?php the_field('field_6534082571f05'); ?>">Donar</a>
     </div>
