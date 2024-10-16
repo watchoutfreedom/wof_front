b514fd032b9459d62314a6bde1cdde1fcc2b3c6b
jano <alex@worldx3.com>
2024-10-16 12:09:38 +0200

    Clean favionc.

--- single_debate.php
+++ single_debate.php
@@ -18,7 +18,8 @@
                   </div>
                 </div>
               </header>
-              <div class="single-thumb"><?php the_post_thumbnail('fotogrande'); ?></div>
+              <div class="single-thumb">
+                <?php the_post_thumbnail('fotogrande'); ?></div>
               <div class="single-default__presentation">
                 <div class="single-default__category">
                   <?php get_template_part('atoms/category-link'); ?>
