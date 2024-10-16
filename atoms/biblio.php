fd6ba0068f78cf12eaafd1bd9823b74f754f7fa7
jano <alex@worldx3.com>
2024-10-16 11:07:40 +0200

    Added bibliografia.

---
+++ atoms/biblio.php
@@ -0,0 +1,12 @@
+<?php if( have_rows('field_637531d2abd69') ): ?>
+    <ul class="slides">
+    <?php while( have_rows('field_637531d2abd69') ): the_row(); 
+        $image = get_sub_field('image');
+        ?>
+        <li>
+            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
+            <p><?php echo acf_esc_html( get_sub_field('caption') ); ?></p>
+        </li>
+    <?php endwhile; ?>
+    </ul>
+<?php endif; ?>
