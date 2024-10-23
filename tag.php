94620f7e765c34dfc00d37c6528dae80a5ba907e
jano <alex@worldx3.com>
2024-10-23 19:06:50 +0200

    Update tags.

--- tag.php
+++ tag.php
@@ -5,7 +5,7 @@
       <?php single_cat_title(); ?> 
     </h1>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <?php get_template_part('components/article-list'); ?>
+      <?php get_template_part('components/product'); ?>
     <?php endwhile; ?>
   </div>
 </section>
