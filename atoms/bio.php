4a944a4b04b5310b014edf8fa282efb0fa19bc00
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-25 01:43:47 +0200

    text

--- atoms/bio.php
+++ atoms/bio.php
@@ -2,13 +2,13 @@
   <div class="meta__author">
     <?php echo get_avatar( get_the_author_meta('email'), '42' ); ?> 
     <div class="meta__content">
-      <div class="meta__name-link"><?php the_author_link(); ?>
+      <div class="meta__name-link"><?php the_author_link(); ?> 
         <?php 
           $organization_name = get_field('Organization', 'user_' . get_the_author_meta('ID'));
           $organization_link = get_field('organization_link', 'user_' . get_the_author_meta('ID'));
           
           if ($organization_name && $organization_link): ?>
-              <a href="<?php echo esc_url($organization_link); ?>" target="_blank"><?php echo esc_html($organization_name); ?></a>
+             - <a href="<?php echo esc_url($organization_link); ?>" target="_blank"><?php echo esc_html($organization_name); ?></a>
         <?php endif; ?>
      </div>
       <p><?php the_author_description(); ?></p>
