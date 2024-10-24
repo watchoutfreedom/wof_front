7657abd51a72f2deba13b815a8aed5e05127b952
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-25 00:16:58 +0200

    donar link

--- atoms/bio.php
+++ atoms/bio.php
@@ -4,7 +4,14 @@
     <div class="meta__content">
       <div class="meta__name-link"><?php the_author_link(); ?></div>
       <p><?php the_author_description(); ?></p>
-      <a href="<?php the_field('field_6534082571f05'); ?>">Donar</a>
+
+      <?php
+      // Check if the 'Donar' field is not empty
+      $donar_link = get_field('field_6534082571f05');
+      if ($donar_link): ?>
+        <a href="<?php echo esc_url($donar_link); ?>">Donar</a>
+      <?php endif; ?>
+
       <a href="<?php the_field('field_670f89bf1a40f'); ?>"><?php the_field('field_670f89bf1a40f'); ?></a>
     </div>
   </div>
