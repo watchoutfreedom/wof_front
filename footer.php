e6de3972b9aeb4d8d976d69095310746bc7f7af3
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-25 12:28:58 +0200

    logged

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <footer class="footer">
-  <p>2024 © Watch Out, Freedom! — <a href="<?php bloginfo('url'); ?>/login">Login</a></p>
+  <p>2024 © Watch Out, Freedom! 
+    <?php if (!is_user_logged_in()): ?>
+      — <a href="<?php echo esc_url(home_url('/login')); ?>">Login</a>
+    <?php endif; ?>
+  </p>
   <p class="mail">mail@watchoutfreedom.com</p>
 </footer>
 
