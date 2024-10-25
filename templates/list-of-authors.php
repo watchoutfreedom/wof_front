f1230e80ace3080b993c78456e6c1e913c69c3b3
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-25 14:00:13 +0200

    enitty

--- templates/list-of-authors.php
+++ templates/list-of-authors.php
@@ -13,11 +13,12 @@ get_header();
 
 <?php
    $display_admins = false;
-   $order_by = 'post_count'; // 'nicename', 'email', 'url', 'registered', 'display_name', or 'post_count'
+   $order_by = 'post_count';
    $order = 'DESC';
-   $role = 'author'; // 'subscriber', 'contributor', 'editor', 'author' - leave blank for 'all'
+   $role = 'author';
    $avatar_size = 161;
-   $hide_empty = false; // hides authors with zero posts
+   $hide_empty = false;
+   
    if(!empty($display_admins)) {
       $blogusers = get_users('orderby='.$order_by.'&role='.$role);
    } else {
@@ -29,6 +30,7 @@ get_header();
       $exclude = implode(',', $exclude);
       $blogusers = get_users('exclude='.$exclude.'&orderby='.$order_by.'&order='.$order.'&role='.$role);
    }
+
    $authors = array();
    $entities = array();
    foreach ($blogusers as $bloguser) {
@@ -45,15 +47,14 @@ get_header();
           $authors[] = (array) $user;
       }
 
-      // Check if 'show_community_page' is checked for the author
-      $entidad = get_user_meta($user->ID, 'entidad', true);
       if ($entidad) {
-            $entities[] = (array) $user;
+          $entities[] = (array) $user;
       }
    }
 
    // Display authors who have 'show_community_page' checked
    if (!empty($authors)) {
+      echo '<h2>Autores</h2>';
       echo '<ul id="grid-contributors">';
       foreach($authors as $author) {
          $display_name = $author['data']->display_name;
@@ -71,6 +72,7 @@ get_header();
 
    // Display entities
    if (!empty($entities)) {
+      echo '<h2>Entidades</h2>';
       echo '<ul id="grid-contributors">';
       foreach($entities as $entitie) {
          $display_name = $entitie['data']->display_name;
@@ -83,7 +85,7 @@ get_header();
       }
       echo '</ul>';
    } else {
-      echo '<p>No contributors found.</p>';
+      echo '<p>No entities found.</p>';
    }
 ?>
 
