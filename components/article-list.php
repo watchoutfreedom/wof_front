248c61a1aeb6c823f212ea37295d7ff904b577a6
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-28 12:52:40 +0100

    respuestas

--- components/article-list.php
+++ components/article-list.php
@@ -11,7 +11,23 @@
       <div class="article-list__date"> <?php the_time('d F Y'); ?></div>
     </a>
       <div class="article-list__responses">
-      
+      <?php 
+        $posts = get_posts(array(
+            'numberposts'   => -1,
+            'post_type'     => 'post',
+            'meta_key'      => 'answer_to',
+            'meta_value'    => get_the_ID(),
+        ));
+
+        if (!empty($posts)) {
+            echo count($posts) . " respuestas";
+        }
+
+        $answer_to = get_field('answer_to');
+        if ($answer_to) {
+            echo " Respuesta a <a class='answer__to' href='" . esc_url(get_permalink($answer_to)) . "'>" . esc_html(get_the_title($answer_to)) . "</a>";
+        }
+        ?>
       </div>
       <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="article-list__link">
         <h3 class="article-list__title">
