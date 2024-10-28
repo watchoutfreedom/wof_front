dbc589fba0d17ecaf2772aa569785554bf46a5f0
jano <alex@worldx3.com>
2024-10-28 09:45:52 +0100

    fix home

--- components/article-list.php
+++ components/article-list.php
@@ -1,5 +1,17 @@
 <article class="article-list">
   <div class="article-list__thumb">
+  <div class="article-list__responses">
+        <?php 
+         $posts = get_posts(array(
+          'numberposts'   => -1,
+          'post_type'     => 'post',
+          'meta_key'      => 'answer_to',
+          'meta_value'    => get_the_ID()
+      ));
+        if(!empty($posts)) echo count($posts)." respuestas";
+        if($answer_to = get_field('answer_to')) echo " Respuesta a <a class='answer__to' href=".get_permalink($answer_to).">".get_the_title($answer_to)."</a>";
+        ?>
+      </div>
     <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="article-list__link">
       <picture class="article-list__picture picture">
         <?php the_post_thumbnail('medium', array( 'class' => 'no-lazy' )); ?>
@@ -16,18 +28,6 @@
         </h3>
       </a>
       <div class="article-list__author"><?php the_author_posts_link(); ?></div>
-      <div class="article-list__responses">
-        <?php 
-         $posts = get_posts(array(
-          'numberposts'   => -1,
-          'post_type'     => 'post',
-          'meta_key'      => 'answer_to',
-          'meta_value'    => get_the_ID()
-      ));
-        if(!empty($posts)) echo count($posts)." respuestas";
-        if($answer_to = get_field('answer_to')) echo " Respuesta a <a class='answer__to' href=".get_permalink($answer_to).">".get_the_title($answer_to)."</a>";
-        ?>
-      </div>
 
     </a>
   </div>
