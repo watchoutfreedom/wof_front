6422f4fb3470f4988d734ee0e692dd55619eb104
jano <alex@worldx3.com>
2024-10-28 09:35:10 +0100

    Reset category:

--- category.php
+++ category.php
@@ -1,22 +1,21 @@
-<?php
-/*
-Template Name: debate
-*/
-get_header();
-?>
-<div class="hero">
-  <h1 class="hero__title"><?php echo single_cat_title(); ?></h1>
-</div>
-<div class="debate__meta">
-  <div class="debate__col">Bote del mes: <span>500 euros</span> (1 de noviembre)</div>
-  <div class="debate__col"><a href="https://dev.watchoutfreedom.com/wp-content/uploads/sites/3/2024/09/Normas.pdf" target="_blank">Ver reglas [PDF]</a></div>
-</div>
+<?php get_header(); ?>
 
-<div class="wrap">
-  <div class="wrap__box wrap__box--debate">
-  <?php echo do_shortcode('[ajax_load_more id="8488605913" loading_style="infinite ring" post_type="post" posts_per_page="8" category="'.get_query_var('category_name').'" transition_container_classes="wrap__box--debate"]')?>
+<section class="section page-sidebar">
+  <div class="page-sidebar__content">
+    <h1 class="page-sidebar__title">
+      <?php single_cat_title(); ?> 
+    </h1>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php get_template_part('components/product'); ?>
+    <?php endwhile; ?>
   </div>
-</div>
+</section>
+
+<?php get_template_part('components/category-nav'); ?>
+
+<?php  else: ?>
+  <?php _e('Lo sentimos, no hay resultados con este término de búsqueda.'); ?>
+<?php endif; ?>
 
 <?php get_template_part('components/colabora'); ?>
 
