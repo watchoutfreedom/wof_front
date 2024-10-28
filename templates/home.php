7b0d5b6eca46c45efdd28e3f9c3afa61a34c31b2
jano <alex@worldx3.com>
2024-10-28 12:26:40 +0100

    New card posts to home

--- templates/home.php
+++ templates/home.php
@@ -31,7 +31,7 @@ get_header();
     <div class="wrap__box wrap__box--news">
       <h3 class="wrap__title">Divulgación</h3>
       <?php $the_query = new WP_Query('showposts=6&category_name=divulgacion'); while ($the_query->have_posts()) : $the_query->the_post();?>
-      <?php get_template_part('components/article-list'); ?>
+      <?php get_template_part('components/debate-list'); ?>
       <?php endwhile; ?>
     </div>
   </div>
@@ -48,7 +48,7 @@ get_header();
     <div class="wrap__box wrap__box--news">
       <h3 class="wrap__title">Novedades WOF</h3>
       <?php $the_query = new WP_Query('showposts=12&category_name=noticias'); while ($the_query->have_posts()) : $the_query->the_post();?>
-      <?php get_template_part('components/article-list'); ?>
+      <?php get_template_part('components/noticia-list'); ?>
       <?php endwhile; ?>
     </div>
   </div>
