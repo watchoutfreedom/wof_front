6e718369ee02801a3ff9f93404f30d4f2fad9905
jano <alex@worldx3.com>
2024-11-06 19:37:17 +0100

    fix author.

--- author.php
+++ author.php
@@ -20,11 +20,13 @@ get_header();
       <?php endif; ?>
     </div>
   </div>
-
+</div>
+<div class="wrap">
   <div class="wrap__box wrap__box--debate">
-    <?php echo do_shortcode('[ajax_load_more id="8488605913" loading_style="infinite ring" author="'.get_the_author_ID().'" post_type="post" posts_per_page="8" category="debate" transition_container_classes="wrap__box--debate"]')?>
+  <?php echo do_shortcode('[ajax_load_more id="8488605913" loading_style="infinite ring" author="'.get_the_author_ID().'" post_type="post" posts_per_page="8" category="debate" transition_container_classes="wrap__box--debate"]')?>
   </div>
 </div>
+
 
 <?php get_template_part('components/colabora'); ?>
 
