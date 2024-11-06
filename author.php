267c6855982bbea7048ae8a275497678671f6e43
jano <alex@worldx3.com>
2024-11-06 19:38:07 +0100

    fix author

--- author.php
+++ author.php
@@ -5,21 +5,8 @@ Template Name: debate
 get_header();
 ?>
 
-<div class="wrap">  
-  <div class="meta__author">
-    <?php echo get_avatar( get_the_author_meta('email'), '42' ); ?> 
-    <div class="meta__content">
-      <?php the_author_posts_link(); ?>
-
-      <?php 
-        $organization_name = get_field('Organization', 'user_' . get_the_author_meta('ID'));
-        $organization_link = get_field('organization_link', 'user_' . get_the_author_meta('ID'));
-        
-        if ($organization_name && $organization_link): ?>
-            <a href="<?php echo esc_url($organization_link); ?>" target="_blank"><?php echo esc_html($organization_name); ?></a>
-      <?php endif; ?>
-    </div>
-  </div>
+<div class="wrap">
+  Autor.
 </div>
 <div class="wrap">
   <div class="wrap__box wrap__box--debate">
