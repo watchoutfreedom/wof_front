524248435bb090f53f1a0aed9d6331d4a1493123
jano <alex@worldx3.com>
2024-11-30 15:07:49 +0100

    fix functions

--- functions.php
+++ functions.php
@@ -273,10 +273,29 @@ function change_role_name() {
   add_action('save_post', 'send_pending_post_email');
 
 /* Limit  excerpt */
-function custom_excerpt_length( $length ) {
-	return 20;
-}
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
-?>
+function excerpt( $limit ) {
+	$excerpt = explode(' ', get_the_excerpt(), $limit);
+	if (count($excerpt)>=$limit) {
+	array_pop($excerpt);
+	$excerpt = implode(" ",$excerpt).'...';
+	} else {
+	$excerpt = implode(" ",$excerpt);
+	}
+	$excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+	return $excerpt;
+	}
+	function content($limit) {
+	$content = explode(' ', get_the_content(), $limit);
+	if (count($content)>=$limit) {
+	array_pop($content);
+	$content = implode(" ",$content).'...';
+	} else {
+	$content = implode(" ",$content);
+	}
+	$content = preg_replace('/[.+]/','', $content);
+	$content = apply_filters('the_content', $content);
+	$content = str_replace(']]>', ']]&gt;', $content);
+	return $content;
+	}?>
 
 
