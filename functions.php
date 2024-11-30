aae81fd216c1e00f9210d70aae4834b6d7f02603
jano <alex@worldx3.com>
2024-11-30 15:05:23 +0100

    test

--- functions.php
+++ functions.php
@@ -272,16 +272,11 @@ function change_role_name() {
   
   add_action('save_post', 'send_pending_post_email');
 
-/**
- * Filter the except length to 20 words.
- *
- * @param int $length Excerpt length.
- * @return int (Maybe) modified excerpt length.
- */
-function wpdocs_custom_excerpt_length( $length ) {
+/* Limit  excerpt */
+function custom_excerpt_length( $length ) {
 	return 20;
 }
-add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 ?>
 
 
