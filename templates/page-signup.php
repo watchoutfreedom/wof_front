ba85452a4fab6dbd9b4e20f597cc3406420b6694
jano <alex@worldx3.com>
2024-11-30 18:30:01 +0100

    fix register

--- templates/page-signup.php
+++ templates/page-signup.php
@@ -10,7 +10,7 @@ if ( is_user_logged_in() )
 acf_form_head();
 
 get_header();?>
-
+<div class="home">
 <div class="wrap wrap--signup">
 
     <?php 
@@ -24,20 +24,15 @@ get_header();?>
     
     
     acf_form([
-        echo '
-        <div class="home">
-        ';
             'id' => 'register_new_user',
             'field_groups' => [ 283 ],
             'honeypot' => true,
             'post_id'      => 'new_user',
             'return' => $redirect,
             'updated_message' => __("Solicitud registrada. Confirma la suscripción en tu email", 'acf'),
-            'submit_value' => __("UNIRME", 'acf')]),
-            echo '
-            </div>
-            ';
+            'submit_value' => __("UNIRME", 'acf')]);
     ?>
 </div>
-
+    </div>
+    
 <?php wp_footer(); ?>
