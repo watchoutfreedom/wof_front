f4d33a26b037137d652b497a9d245b4ecfb2e95f
noauthoritybuturself <nobordersxnonations@disroot.org>
2024-12-10 18:58:02 +0100

    recover password

--- templates/page-login.php
+++ templates/page-login.php
@@ -116,7 +116,6 @@ if ( ! is_user_logged_in() ) {
           <?php
         } else {
           // Display an error message if the key is invalid or the user does not exist
-          print_r(get_user_meta( $user->ID, 'reset_key', true ));
           echo '<p>Invalid password reset key.</p>';
         }
     ?>
