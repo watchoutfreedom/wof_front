3bf108d97f6990fba2fc74f5efc9b9b83d680aba
jano <alex@worldx3.com>
2025-01-23 18:43:48 +0100

    Last news

--- components/list-noticias.php
+++ components/list-noticias.php
@@ -4,6 +4,23 @@
     <h3 class="page-sidebar__title">
       Últimas noticias
     </h3>
+
+
+    <?php
+    $current_post_id = get_the_ID();
+    $the_query = new WP_Query( array(
+        'showposts'   => 8,,
+        'category_name' = 'noticias',
+        'post__not_in' => array( $current_post_id )
+    ) );
+
+    while ( $the_query->have_posts() ) : $the_query->the_post();
+        get_template_part( 'components/article-list' );
+    endwhile;
+
+    wp_reset_postdata();
+    ?>
+
     <?php $the_query = new WP_Query('showposts=4&category_name=noticias'); while ($the_query->have_posts()) : $the_query->the_post();?>
     <?php get_template_part('components/article-list'); ?>
     <?php endwhile; ?>
