08b9618b1a2339be4947988cac32ab727ecff8c4
watchoutfreedom <watchoutfreedom@gmail.com>
2025-10-23 10:49:17 +0200

    unete

--- templates/home.php
+++ templates/home.php
@@ -7,7 +7,7 @@ get_header();
 <div class="home">
 <div class="hero">
     <h1 class="hero__title"><a class="" href="https://wofreedom.org/divulgacion/">Conoce.</a> <a href="https://wofreedom.org/debate/">Debate.</a> <a href="https://wofreedom.org/colabora">Colabora.</a></h1>
-    <div class="hero__description">La vida en sociedad necesita puntos de encuentro. Únete la comunidad Wof!.</div>
+    <div class="hero__description">La vida en sociedad necesita puntos de encuentro.</div>
   </div>
   <div class="wrap">
     <div class="wrap__box wrap__box--author">
