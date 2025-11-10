aa981c8c22d319f2da15eff7b891800d027494d8
watchoutfreedom <watchoutfreedom@gmail.com>
2025-11-10 12:45:53 +0100

    logo

--- category.php
+++ category.php
@@ -7,7 +7,7 @@
       // Check if we are on the 'helpbuttons' category page
       if ( is_category('helpbuttons') ) {
         // If yes, display the image instead of the title
-        echo '<img class="category-title-image" src="https://wofreedom.org/wp-content/uploads/sites/8/2025/11/hb_logo_hor_00.png" alt="' . single_cat_title('', false) . '">';
+        echo '<img class="category-title-image" src="https://wofreedom.org/wp-content/uploads/sites/8/2025/11/hb_logo_hor_00-1.png" alt="' . single_cat_title('', false) . '">';
       } else {
         // Otherwise, display the normal category title
         single_cat_title();
